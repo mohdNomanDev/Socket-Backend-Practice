2bd4625ff372a0930d5904cd33cbc3a84a08d4d4
Mohd Noman <mohdnoman0302@gmail.com>
2026-03-07 16:51:07 +0530

    practicing socket events: emit and on

--- socketHandler.js
+++ socketHandler.js
@@ -3,8 +3,18 @@ export default function socketHandler(io) {
   io.on('connection', (socket) => {
     console.log('A user connected');
 
+    socket.on('message', (msg) => {
+      console.log('Received message:', msg);
+      io.emit('message', 'Hii Socket'); // Broadcast the message to all clients
+    });
+
+    socket.on('chats', (msg) => {
+        console.log(msg);
+        socket.emit('reply', {name: 'Mohd Noman', chats: 'Hello bro'});
+    })
     socket.on('disconnect', () => {
       console.log('A user disconnected');
     });
 });
 }
+
